bfeebc1b9c41b01c1112f97893a37deb3c07899e
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-10 18:41:49 +0100

    Add files via upload

---
+++ Jackaboi Bot 1.0.2 - Copy.py
@@ -0,0 +1,77 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "+")
+
+@client.event
+async def on_ready():
+    print("Thankyou For Using Jackaboi Bot!")
+    await client.change_presence(game=discord.Game(name="+commands | Still In BETA!"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('+hello'):
+        msg = 'Hello {0.author.mention} How Are You Today'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+bye'):
+        msg = 'Goodbye {0.author.mention} Hope To See You Soon :wave:'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+help'):
+        msg = 'Contacting The <@344967220025098242> Someone Needs Help!'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+commands'):
+        msg = ' {0.author.mention} The Bot Commands are `+hello` `+bye` `+mute` `+unmute` `+help` `website` `+invite` `+feedback` `+commands` More Comeing Soon!'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+invite'):
+        msg = 'Want To Invte Me To Your Discord Press This Link https://goo.gl/94ZSqo'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+feedback'):
+        msg = 'To Give Feedback Join The Creators Discord https://discord.gg/hbgVy88'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('+website'):
+        msg = 'Checkout Our Website http://skyhaven.ga'.format(message)
+        await client.send_message(message.channel, msg)
+    await client.process_commands(message)
+
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Jackaboi Bot Is Now Online!")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+
+@client.command(pass_context = True)
+async def mute(ctx, member: discord.Member, time: int, *,reason: str):
+    em = discord.Embed(title="User muted!", description=None, color=0x20f911)
+    em.add_field(name="User:",value=f"{member}")
+    em.add_field(name="Time: ", value=f"{time} min")
+    em.add_field(name="Reason: ", value=f"{reason}")
+    em.set_thumbnail(url=member.avatar_url)
+    role = discord.utils.get(member.server.roles, name="Muted")
+    time_min = time * 60
+    if ctx.message.author.server_permissions.administrator:
+        await client.add_roles(member, role)
+        await client.say(" {} Has been muted for {} minutes! :white_check_mark:".format(member.mention, time))
+        await asyncio.sleep(time_min)
+        await client.remove_roles(member, role)
+        await client.say("{} has been unmuted! :smiley:".format(member))     
+    else:
+        await client.say("You don't have permission to execute this command! :stuck_out_tongue: ")
+
+@client.command(pass_context=True)
+async def unmute(ctx, member: discord.Member):
+    role = discord.utils.get(member.server.roles, name="Muted")
+    if ctx.message.author.server_permissions.administrator:
+        await client.remove_roles(member, role)
+        await client.say("{} has been unmuted! :white_check_mark: ".format(member.mention))
+    else:
+        await client.say("You don't have permissions to execute these command! :stuck_out_tongue: ")
+        
+client.loop.create_task(list_servers())
+client.run ("BOT TOKEN")
