604cd9c735fdf3197c00ca932ee63c6238411c8f
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-13 04:47:00 +0100

    Update Jackaboi_Bot_1,0,2.py

--- Jackaboi_Bot_1,0,2.py
+++ Jackaboi_Bot_1,0,2.py
@@ -12,7 +12,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print("Thankyou For Using Jackaboi Bot!")
-    await client.change_presence(game=discord.Game(name="+commands | Now Online 24/7!"))
+    await client.change_presence(game=discord.Game(name="+commands | Olive Oil ❤"))
 
 @client.event
 async def on_message(message):
