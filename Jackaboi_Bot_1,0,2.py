c09ead00968d18d87c947e7548f4c9d50dd211a8
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-16 14:29:01 +0100

    Update Jackaboi_Bot_1,0,2.py

--- Jackaboi_Bot_1,0,2.py
+++ Jackaboi_Bot_1,0,2.py
@@ -37,6 +37,13 @@ async def on_message(message):
     if message.content.startswith('+website'):
         msg = 'This Command Is Disabled By Jackaboi#8319'.format(message)
         await client.send_message(message.channel, msg)
+    
+    if message.content.startswith('+test') and message.author.id == 338071152213753867:
+        await bot.say ("www.google.com")
+    else:
+         await client.send("You Cannot Use This Command")
+    
+    
     await client.process_commands(message)
    
     if message.content.startswith('+say'):
