42d95eb723b2a2527cf46d6d3a80b024a3b7ef7c
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-29 16:09:04 +0100

    Update Jackaboi_Bot_1,0,3.py

--- Jackaboi_Bot_1,0,3.py
+++ Jackaboi_Bot_1,0,3.py
@@ -68,7 +68,7 @@ async def on_message(message):
                     await client.send_message(message.channel, "<@%s> awww you lost :cry:" % (message.author.id))
          
     if message.content.startswith(';version'):
-        msg = 'V1.0.4 BETA\nBuild:101'.format(message)
+        msg = 'V1.0.4 BETA\nBuild:102'.format(message)
         await client.send_message(message.channel, msg)    
        
        
@@ -142,7 +142,7 @@ async def jack(ctx):
         return await client.say("Only Jackaboi#8319 Can Use This Command!")
 @client.command(pass_context=True)
 async def help(ctx):
-    embed=discord.Embed(title="Help | Info", description="""Bot Prefix ;
+    embed=discord.Embed(title="Help | Info", description="""***Bot Prefix ;***
 hello
 bye
 say
