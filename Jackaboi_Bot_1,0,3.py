2280e30349da67aba6924c6d1204403094f221fc
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-30 15:56:38 +0100

    Update Jackaboi_Bot_1,0,3.py

--- Jackaboi_Bot_1,0,3.py
+++ Jackaboi_Bot_1,0,3.py
@@ -158,10 +158,10 @@ More Coming Soon!""", color=0x66009D)
     await client.say(embed=embed)
 @client.command(pass_context=True)
 async def version(ctx):
-    embed=discord.Embed(title="Info", description="V1.0.4 Beta | Build:112", color=0x66009D)
+    embed=discord.Embed(title="Info", description="V1.0.4 Beta | Build:113", color=0x66009D)
     await client.say(embed=embed)
 
-   @client.command(pass_context=True)
+@client.command(pass_context=True)
 async def unmute(ctx, member: discord.Member):
     role = discord.utils.get(member.server.roles, name="Muted")
     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '344967220025098242':
